08a6465a53675d933d95f2a6cbc04bd65effb23b
Jitendra Kumar <jitendrasahu6761@gmail.com>
2023-04-09 12:08:29 +0530

    initial commit with a fress start

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
-import Main from "./Components/Main";
+// import Main from "./Components/Main";
 import Learnings from "./Components/Learnings";
-import Footer from "./Components/Footer";
+// import Footer from "./Components/Footer";
 
 function App() {
-  // a simple object
-  let info = {
-    name: "Jitendra Kumar",
-    age: 21,
-    profession: "web dev",
-  };
   return (
     <React.Fragment>
       <Navbar />
